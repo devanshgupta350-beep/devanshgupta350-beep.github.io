e446413a8c34db3f6a62d99afb3803cd01a07512
devanshgupta350-beep <devanshgupta350@gmail.com>
2026-02-09 18:14:05 +0530

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -46,3 +46,22 @@ window.addEventListener("scroll", () => {
   });
 });
 
+// Scroll Reveal
+const reveals = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  const windowHeight = window.innerHeight;
+  const revealPoint = 120;
+
+  reveals.forEach(el => {
+    const revealTop = el.getBoundingClientRect().top;
+
+    if (revealTop < windowHeight - revealPoint) {
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
+
